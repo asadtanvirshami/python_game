f4ce3bada69331e0d101fb9dd71d92d8a5e863c9
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2023-06-09 18:11:57 +0500

    min_max implemantation

---
+++ min_max.py
@@ -0,0 +1,23 @@
+numbers = [5, 2, 9, 1, 7]
+
+# Find the minimum value
+minimum = min(numbers)
+print("Minimum:", minimum)  # Output: 1
+
+# Find the maximum value
+maximum = max(numbers)
+print("Maximum:", maximum)  # Output: 9
+
+# Minimum and maximum in a tuple
+my_tuple = (3, 1, 6, 2)
+minimum = min(my_tuple)
+maximum = max(my_tuple)
+print("Minimum:", minimum)  # Output: 1
+print("Maximum:", maximum)  # Output: 6
+
+# Minimum and maximum in a string
+my_string = "hello"
+minimum = min(my_string)
+maximum = max(my_string)
+print("Minimum:", minimum)  # Output: 'e'
+print("Maximum:", maximum)  # Output: 'o'
